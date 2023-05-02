8bbc1d2b6f3d7067b0e844cf932108138ac05a6a
aminafayaz <128042662+aminafayaz@users.noreply.github.com>
2023-05-02 22:11:32 +0530

    chenges made in one.dart

--- collab_flutter_project/lib/one.dart
+++ collab_flutter_project/lib/one.dart
@@ -6,8 +6,26 @@ class Firstpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 233, 233, 229),
       appBar: AppBar(
-        title: const Text("Amina's Page"),
+        title: const Center(
+
+          child: Text('A M I N A'),
+        
+        ),
+
+
+        titleTextStyle: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.white,),
+        backgroundColor:const Color.fromARGB(255, 25, 50, 88),
+        ),
+
+      
+        floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromARGB(255, 25, 50, 88),
+        onPressed: () {
+          Navigator.pop(context, "Returned from Amina's Page");
+        },
+        child: const Icon(Icons.home),
       ),
     );
   }
