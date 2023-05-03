fe1a1e7b9a8302841e25e6f39461a59f2d8a88d9
aminafayaz <128042662+aminafayaz@users.noreply.github.com>
2023-05-03 22:28:07 +0530

    Revert "removed the cat animation"
    This reverts commit 58517e93f57e79c01bc472e06d837901d628df74.

--- collab_flutter_project/lib/one.dart
+++ collab_flutter_project/lib/one.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:lottie/lottie.dart';
 
 class Firstpage extends StatelessWidget {
   const Firstpage({super.key});
@@ -10,26 +10,26 @@ class Firstpage extends StatelessWidget {
       backgroundColor: Color.fromARGB(255, 12, 43, 88),
       appBar: AppBar(
         title: const Center(
+
           child: Text('A M I N A  :)'),
+        
         ),
 
-        titleTextStyle: const TextStyle(
-          fontSize: 32,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
 
+        titleTextStyle: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.white,),
         backgroundColor: Colors.black,
-      ),
+        ),
 
-      floatingActionButton: FloatingActionButton(
+      
+        floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.black,
         onPressed: () {
           Navigator.pop(context, "Returned from Amina's Page");
         },
         child: const Icon(Icons.home),
       ),
+body: Center(child: Lottie.network('https://assets6.lottiefiles.com/packages/lf20_jpxsQh.json'),),
+
     );
-    
   }
 }
