88dade008b2d339e266ff8c9538327a177d29e8d
jydv402 <jydv402@gmail.com>
2023-05-04 21:42:48 +0530

    Did minor changes to two.dart file

--- collab_flutter_project/lib/two.dart
+++ collab_flutter_project/lib/two.dart
@@ -6,16 +6,30 @@ class Secondpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.deepPurple[300],
       appBar: AppBar(
+        backgroundColor: Colors.deepPurple[700],
         title: const Text("Jayadev's Page"),
+        centerTitle: true,
+        elevation: 0,
+        titleTextStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
       ),
       body: const Center(
         child: Text("This is my page!"),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.close),
+        onPressed: () {
+          Navigator.pop(context, "Returned from Jayadev's Page");
+        },
+        backgroundColor: Colors.deepPurple[700],
+        hoverColor: Colors.red,
+        child: const Icon(Icons.home_filled),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
