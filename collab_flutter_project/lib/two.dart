d7de25b20eda976986fe521f7cce87e5f8517880
jydv402 <jydv402@gmail.com>
2023-05-05 12:41:06 +0530

    Added random Gradient background

--- collab_flutter_project/lib/two.dart
+++ collab_flutter_project/lib/two.dart
@@ -1,35 +1,73 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class Secondpage extends StatelessWidget {
+class Secondpage extends StatefulWidget {
   const Secondpage({super.key});
 
   @override
+  State<Secondpage> createState() => _SecondpageState();
+}
+
+class _SecondpageState extends State<Secondpage> {
+  Random r = Random();
+
+  int clrA = 0xffab68f2;
+  int clrB = 0xff90fcda;
+  int val = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[300],
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple[700],
-        title: const Text("Jayadev's Page"),
-        centerTitle: true,
-        elevation: 0,
-        titleTextStyle: const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple[500],
+          title: const Text("Jayadev's Page"),
+          centerTitle: true,
+          titleTextStyle: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
-      ),
-      body: const Center(
-        child: Text("This is my page!"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context, "Returned from Jayadev's Page");
-        },
-        backgroundColor: Colors.deepPurple[700],
-        hoverColor: Colors.red,
-        child: const Icon(Icons.home_filled),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [Color(clrA), Color(clrB)])),
+        ),
+        floatingActionButton: Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned(
+              right: 20,
+              bottom: 30,
+              child: FloatingActionButton(
+                elevation: 10,
+                onPressed: () {
+                  Navigator.pop(context, "Returned from Jayadev's Page");
+                },
+                backgroundColor: Colors.deepPurple[500],
+                hoverColor: Colors.red,
+                child: const Icon(
+                  Icons.home_filled,
+                ),
+              ),
+            ),
+            Positioned(
+              right: 20,
+              bottom: 110,
+              child: FloatingActionButton(
+                elevation: 10,
+                onPressed: () {
+                  setState(() {
+                    clrA = r.nextInt(0xffffffff);
+                    clrB = r.nextInt(0xffffffff);
+                  });
+                },
+                backgroundColor: Colors.deepPurple[500],
+                hoverColor: Colors.green[400],
+                child: const Icon(
+                  Icons.restart_alt_sharp,
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
