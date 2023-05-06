b75d1a06772367d13646d3dc7acbbd68ed9d49c0
aminafayaz <128042662+aminafayaz@users.noreply.github.com>
2023-05-06 17:45:00 +0530

    confetti

--- collab_flutter_project/lib/one.dart
+++ collab_flutter_project/lib/one.dart
@@ -1,3 +1,6 @@
+
+import 'package:lottie/lottie.dart';
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 
 class Firstpage extends StatefulWidget {
@@ -8,47 +11,87 @@ class Firstpage extends StatefulWidget {
 }
 
 class _FirstpageState extends State<Firstpage> {
+  bool isPlaying = false;
+  final _controller = ConfettiController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller.addListener(() {
+      setState(() {
+        isPlaying = _controller.state == ConfettiControllerState.playing;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Color.fromARGB(255, 12, 43, 88),
-        appBar: AppBar(
-          title: const Center(
-            child: Text('A M I N A :)'),
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Scaffold(
+          backgroundColor: Color.fromARGB(255, 12, 43, 88),
+          appBar: AppBar(
+            title: const Center(
+              child: Text('A M I N A :)'),
+            ),
+            titleTextStyle: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            backgroundColor: Colors.black,
           ),
-          titleTextStyle: const TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.black,
+            onPressed: () {
+              Navigator.pop(context, "Returned from Amina's Page");
+            },
+            child: const Icon(Icons.home),
           ),
-          backgroundColor: Colors.black,
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.black,
-          onPressed: () {
-            Navigator.pop(context, "Returned from Amina's Page");
-          },
-          child: const Icon(Icons.home),
-        ),
-        body: Container(
-          child: Center(
-            child: ElevatedButton(
-              onPressed: () {},
+          body: Row(children: [
+            Lottie.network(
+                'https://assets6.lottiefiles.com/packages/lf20_jpxsQh.json'),
+            ElevatedButton(
+              onPressed: () {
+                if (isPlaying) {
+                  _controller.stop();
+                } else {
+                  _controller.play();
+                }
+              },
               style: ElevatedButton.styleFrom(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
                 backgroundColor: Colors.black,
                 shape: const StadiumBorder(),
               ),
-              child: const Text(
-                'CLICK HERE ',
-                style: TextStyle(
+              child: Text(
+                isPlaying ? 'STOP' : 'CLICK HERE !',
+                style: const TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                     fontSize: 20),
               ),
             ),
-          ),
-        ));
+          ]),
+        ),
+        ConfettiWidget(
+          confettiController: _controller,
+          shouldLoop: true,
+          blastDirectionality: BlastDirectionality.explosive,
+          gravity: 0.01,
+          emissionFrequency: 0.01,
+          numberOfParticles: 30,
+        ),
+      ],
+    );
   }
 }
