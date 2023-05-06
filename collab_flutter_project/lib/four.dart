85815f1abd64829939946097486762f4adf5f735
Karthi-R-K <karthi102004@gmail.com>
2023-05-07 01:45:53 +0530

    added animation

--- collab_flutter_project/lib/four.dart
+++ collab_flutter_project/lib/four.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class Fourthpage extends StatelessWidget {
   const Fourthpage({super.key});
@@ -6,13 +7,51 @@ class Fourthpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.lightGreenAccent, Colors.tealAccent])),
+        child: Stack(children: [
+          Center(
+            child: Lottie.network(
+                'https://assets10.lottiefiles.com/packages/lf20_U6OKyytl3f.json'),
+          ),
+        ]),
+      ),
+      backgroundColor: const Color.fromARGB(255, 204, 255, 64),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.brown,
+        onPressed: () {
+          Navigator.pop(context, "Returned from Karthik's Page");
+        },
+        child: const Icon(Icons.home),
+      ),
       appBar: AppBar(
-          centerTitle: true,
-          title: const Text("Karthik's Page",
-              style: TextStyle(
-                  color: Colors.deepPurple,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 33))),
+        centerTitle: true,
+        title: const Text(
+          "Karthik's Page ;)",
+          style: TextStyle(
+            color: Colors.deepPurpleAccent,
+            fontWeight: FontWeight.bold,
+            fontSize: 33,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        backgroundColor: Colors.amberAccent,
+        elevation: 13,
+        shadowColor: Colors.deepOrangeAccent,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+        ),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 248, 205, 48),
+              Color.fromARGB(255, 219, 68, 118)
+            ]),
+          ),
+        ),
+      ),
     );
   }
 }
