94e908a9f48f998f8ab6889f10f69cb03c0de6db
jydv402 <jydv402@gmail.com>
2023-05-09 22:19:13 +0530

    Minor UI changes

--- collab_flutter_project/lib/two.dart
+++ collab_flutter_project/lib/two.dart
@@ -22,12 +22,17 @@ class _SecondpageState extends State<Secondpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        extendBodyBehindAppBar: true,
-        drawer: Drawer(
+      extendBodyBehindAppBar: true,
+      drawer: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Drawer(
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.elliptical(18, 18))),
             backgroundColor: Colors.white,
             elevation: 0,
             child: Container(
               decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.elliptical(18, 18)),
                 gradient: LinearGradient(colors: [Color(clrB), Color(clrA)]),
               ),
               child: ListView(
@@ -62,92 +67,94 @@ class _SecondpageState extends State<Secondpage> {
                 ],
               ),
             )),
-        appBar: AppBar(
-          //Blurred appbar
-          iconTheme: const IconThemeData(color: Colors.black38),
-          elevation: 0,
-          flexibleSpace: ClipRect(
-              child: BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: 10,
-              sigmaY: 10,
-            ),
-            child: Container(
-              color: Colors.transparent,
-            ),
-          )),
-          backgroundColor: Colors.white.withAlpha(140),
-          title: const Text("The Gradient Isle"),
-          centerTitle: true,
-          titleTextStyle: const TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.w700,
-            color: Colors.black,
+      ),
+      appBar: AppBar(
+        //Blurred appbar
+        iconTheme: const IconThemeData(color: Colors.black38),
+        elevation: 0,
+        flexibleSpace: ClipRect(
+            child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 10,
+            sigmaY: 10,
           ),
+          child: Container(
+            color: Colors.transparent,
+          ),
+        )),
+        backgroundColor: Colors.white.withAlpha(140),
+        title: const Text("The Gradient Isle"),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.w700,
+          color: Colors.black,
         ),
-        body: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [Color(clrA), Color(clrB)])),
-          child: Stack(children: [
-            Center(
-              child: Lottie.network(
-                'https://assets1.lottiefiles.com/packages/lf20_96bovdur.json',
-                alignment: const Alignment(0, 0),
-                height: MediaQuery.of(context).size.height * 0.65,
-                width: MediaQuery.of(context).size.width * 0.65,
+      ),
+      floatingActionButton: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            right: 20,
+            bottom: 30,
+            child: FloatingActionButton(
+              elevation: 10,
+              onPressed: () {
+                Navigator.pop(context, "Returned from Jayadev's Page");
+
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text("Returned to Homepage"),
+                  duration: Duration(milliseconds: 500),
+                  behavior: SnackBarBehavior.fixed,
+                ));
+              },
+              backgroundColor: Colors.deepPurple[500],
+              hoverColor: Colors.red,
+              child: const Icon(
+                Icons.home_filled,
               ),
             ),
-          ]),
-        ),
-        floatingActionButton: Stack(
-          fit: StackFit.expand,
-          children: [
-            Positioned(
-              right: 20,
-              bottom: 30,
-              child: FloatingActionButton(
-                elevation: 10,
-                onPressed: () {
-                  Navigator.pop(context, "Returned from Jayadev's Page");
-
+          ),
+          Positioned(
+            right: 20,
+            bottom: 110,
+            child: FloatingActionButton(
+              elevation: 10,
+              onPressed: () {
+                setState(() {
+                  clrA = r.nextInt(0xffffffff);
+                  clrB = r.nextInt(0xffffffff);
+                  //Snackbar
                   ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text("Returned to Homepage"),
-                    duration: Duration(milliseconds: 500),
+                    content: Text("Changed Background Gradient"),
+                    duration: Duration(milliseconds: 350),
                     behavior: SnackBarBehavior.fixed,
                   ));
-                },
-                backgroundColor: Colors.deepPurple[500],
-                hoverColor: Colors.red,
-                child: const Icon(
-                  Icons.home_filled,
-                ),
+                });
+              },
+              backgroundColor: Colors.deepPurple[500],
+              hoverColor: Colors.green[400],
+              child: const Icon(
+                Icons.restart_alt_sharp,
               ),
             ),
-            Positioned(
-              right: 20,
-              bottom: 110,
-              child: FloatingActionButton(
-                elevation: 10,
-                onPressed: () {
-                  setState(() {
-                    clrA = r.nextInt(0xffffffff);
-                    clrB = r.nextInt(0xffffffff);
-                    //Snackbar
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text("Changed Background Gradient"),
-                      duration: Duration(milliseconds: 350),
-                      behavior: SnackBarBehavior.fixed,
-                    ));
-                  });
-                },
-                backgroundColor: Colors.deepPurple[500],
-                hoverColor: Colors.green[400],
-                child: const Icon(
-                  Icons.restart_alt_sharp,
-                ),
-              ),
-            )
-          ],
-        ));
+          )
+        ],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Color(clrA), Color(clrB)])),
+        child: Stack(children: [
+          Center(
+            child: Lottie.network(
+              'https://assets1.lottiefiles.com/packages/lf20_96bovdur.json',
+              alignment: const Alignment(0, 0),
+              height: MediaQuery.of(context).size.height * 0.65,
+              width: MediaQuery.of(context).size.width * 0.65,
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
